4a7ff8086c9e0ac9f127e00534f06b3a14b10ce5
Aaron Kofsky <a2aarontothe2@gmail.com>
2018-07-07 00:59:39 -0400

    Add more names, add location/drop messages

--- fortnite.py
+++ fortnite.py
@@ -1,10 +1,25 @@
 import random
 
-def random_phrase():
-    fort_puns = ["Fort", "Short", "Sport", "Splort", "Fight", "Fork", "Thwart", "Court", "Port",
-                 "Voret", "4t", "Four", "Ford", "Dork", "Snort", "Bork", "Quart", "Sort"]
-    nite_puns = ["nite", "night", "knight", "fight", "kite", "blight", "bike", "smite", "write", "quite", "might", "slight"
-                 "knife", "life", "strike", "rife", "height", "right", "bright", "light", "lite", "bite", "byte", "flight"]
+def random_forkknife():
+    common_fort = ["Bork", "Cork", "Court", "Dork", "Fork", "Fort", "Port",
+        "Quart", "Short", "Slorp", "Snort", "Sort", "Splort", "Sport",]
+    uncommon_fort = ["4t", "Abort", "Force", "Ford", "Forest", "Four", "Fourty",
+        "Horse", "New York", "Pitchfork", "Thwart", "Voret", "Ψ"]
+    common_nite = ["bike", "bite", "blight", "bright", "byte", "fight",
+        "flight", "height", "kite", "knife", "knight", "life", "light","lite",
+        "might", "night", "nite", "quite", "rife", "right", "slight", "smite",
+        "sprite", "strike", "write",]
+    uncommon_nite = ["invite", "despite", "goodnight", "excite", "ignite",
+        "alight", "upright", "overwrite", "allright", "unite", "tonight", "🔪"]
+    fort = random.choice(common_fort) if random.random() > 0.1 else random.choice(uncommon_fort)
+    nite = random.choice(common_nite) if random.random() > 0.1 else random.choice(uncommon_nite)
+
+    if random.random() > 0.99:
+        return "🍴"
+     
+    return fort + nite
+
+def random_about_to_play():
     intro_statements = ["I'm about to play some ", "Gonna get on ", "Gettin on ", "Gonna queue up for some "]
     ending_statements = [" real soon.", ".", " right now.", " soon."]
     intro_questions = ["Anyone want to play some ",
@@ -16,9 +31,49 @@ def random_phrase():
     ending_questions = [" with me?", " soon?", " right now?", " real soon?", "?"]
 
     if random.randint(0, 1) == 0:
-        return random.choice(intro_statements) + random.choice(fort_puns) + random.choice(nite_puns) + random.choice(ending_statements)
+        return random.choice(intro_statements) + random_forkknife() + random.choice(ending_statements)
     else:
-        return random.choice(intro_questions) + random.choice(fort_puns) + random.choice(nite_puns) + random.choice(ending_questions)
+        return random.choice(intro_questions) + random_forkknife() + random.choice(ending_questions)
+
+def random_drop():
+    group_names = ["boys", "girls", "enbies", 
+                   "folks", "folxs", "comrades", "mortals"]
+    sentances = ["Alright, SQUAD, where we dropping?",
+                 "Where we droppin SQUAD?",
+                 "You SQUAD ready to drop at LOCATION?",
+                 "Yo SQUAD, we droppin at LOCATION?"]
+    sentance = random.choice(sentances)
+    sentance = sentance.replace("SQUAD", random.choice(group_names))
+    sentance = sentance.replace("LOCATION", random_location())
+    return sentance
+
+def random_location():
+    adjectives =  ["Anarchy", "Dusty", "Fatal", "Flush", "Greasy",
+                   "Haunted", "Junk", "Lonely", "Loot", "Lucky", "Moisty",
+                   "Pleasant", "Retail", "Risky", "Shifty", "Snobby", "Tomato"]
+    locations = ["Acres", "Divot", "Fields", "Factory",
+                 "Grove", "Hills", "Junction" "Lodge", "Lake",
+                 "Landing", "Mire", "Park", "Row", "Reels",
+                 "Shafts", "Shores", "Town"]
+
+    special_names = ["the crates",
+                     "the bridge",
+                     "that hill",
+                     "the house on the hill",
+                     "the worst possible location",
+                     "that town",]
+
+    if random.random() > 0.1:
+        return random.choice(adjectives) + " " + random.choice(locations)
+    else:
+        return random.choice(special_names)
+
+def random_phrase():
+    choice = random.randint(0, 1)
+    if choice == 0:
+        return random_about_to_play()
+    else:
+        return random_drop()
 
 if __name__ == '__main__':
     print(random_phrase())
